e79e8e61601e265de04f04e4f3d0466a7982b83e
Michael Campbell <Mcampbell6091@conestogac.on.ca>
2024-10-31 19:57:38 -0400

    Commit # 5 - Invalid Test cases added

--- Assignment3(1)/main.c
+++ Assignment3(1)/main.c
@@ -9,11 +9,15 @@ const char* PickWinner(char array1[], char array2[])
 	{
 		strcpy_s(winner, 10, "Player1");
 	}
-	if ((array1 == "scissors" && array2 == "rock") || (array1 == "paper" && array2 == "scissors") || (array1 == "rock" && array2 == "paper"))
+	else if ((array1 == "scissors" && array2 == "rock") || (array1 == "paper" && array2 == "scissors") || (array1 == "rock" && array2 == "paper"))
 	{
 		strcpy_s(winner, 10, "Player2");
 	}
-	if ((array1 == "rock" && array2 == "rock") || (array1 == "paper" && array2 == "paper") || (array1 == "scissors" && array2 == "scissors"))
+	else if ((array1 == "rock" && array2 == "rock") || (array1 == "paper" && array2 == "paper") || (array1 == "scissors" && array2 == "scissors"))
+	{
+		strcpy_s(winner, 10, "Draw");
+	}
+	else if (array1 != "rock" || array2 != "rock" || array1 != "paper" || array2 != "paper" || array1 != "scissors"  || array2 != "scissors")
 	{
 		strcpy_s(winner, 10, "Invalid");
 	}
--- TestCases/TestCases.cpp
+++ TestCases/TestCases.cpp
@@ -17,7 +17,7 @@ namespace TestCases
 			const char* actual_result = PickWinner("rock", "scissors");
 			Assert::AreSame(expected_result, actual_result);
 		}
-		TEST_METHOD(Player2_Winning_Test)
+		TEST_METHOD(Player2_Wining_Test)
 		{
 			const char* expected_result = "Player2";
 			const char* actual_result = PickWinner("scissors", "paper");
@@ -25,8 +25,20 @@ namespace TestCases
 		}
 		TEST_METHOD(Draw_Test)
 		{
-			const char* expected_result = "Ivalid";
+			const char* expected_result = "Draw";
 			const char* actual_result = PickWinner("paper", "paper");
+			Assert::AreSame(expected_result, actual_result);
+		}
+		TEST_METHOD(Invalid_Test)
+		{
+			const char* expected_result = "Invalid";
+			const char* actual_result = PickWinner("steve", "paper");
+			Assert::AreSame(expected_result, actual_result);
+		}
+		TEST_METHOD(Invalid_Test2)
+		{
+			const char* expected_result = "Invalid";
+			const char* actual_result = PickWinner("ROCK", "paper");
 			Assert::AreSame(expected_result, actual_result);
 		}
 	};
